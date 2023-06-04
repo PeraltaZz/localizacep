ea287034c35330e53a13d217b73c4d4015bece3b
Brenno Peralta <brenno.peraltaa@gmail.com>
2023-06-03 21:40:39 -0400

    update: DescobrirEndereco componente

--- src/components/Endereco/DescobrirEndereco.jsx
+++ src/components/Endereco/DescobrirEndereco.jsx
@@ -1,41 +1,41 @@
 import React, { useState } from "react";
+import DataDisplayEndereco from "./DataDisplayEndereco";
 
-const DescobrirEndereco= () => {
-    const [cep, setCep] = useState("");
-    const [buttonClicked, setButtonClicked] = useState(false);
-  
-    const buscarEndereco = (e) => {
-      e.preventDefault();
-      setButtonClicked(true);
-    };
-  
-    const cepChange = (e) => {
-      setCep(e.target.value);
-      setButtonClicked(false);
-    };
-  
-    return (
-      <div className="descobrir-endereco d-flex-column g-30">
-        <form className="d-flex-column g-30">
-          <div className="form-row">
-            <label htmlFor="cep">CEP</label>
-            <input
-              className="input-style"
-              type="text"
-              name="cep"
-              id="cep"
-              value={cep}
-              onChange={cepChange}
-            />
-          </div>
-          <button className="button-style" onClick={buscarEndereco}>
-            CONSULTAR
-          </button>
-        </form>
-       
-      </div>
-    );
+const DescobrirEndereco = () => {
+  const [cep, setCep] = useState("");
+  const [buttonClicked, setButtonClicked] = useState(false);
+
+  const buscarEndereco = (e) => {
+    e.preventDefault();
+    setButtonClicked(true);
   };
-  
-  export default DescobrirEndereco;
-  
+
+  const cepChange = (e) => {
+    setCep(e.target.value);
+    setButtonClicked(false);
+  };
+
+  return (
+    <div className="descobrir-endereco d-flex-column g-30">
+      <form className="d-flex-column g-30">
+        <div className="form-row">
+          <label htmlFor="cep">CEP</label>
+          <input
+            className="input-style"
+            type="text"
+            name="cep"
+            id="cep"
+            value={cep}
+            onChange={cepChange}
+          />
+        </div>
+        <button className="button-style" onClick={buscarEndereco}>
+          CONSULTAR
+        </button>
+      </form>
+      <DataDisplayEndereco cep={cep} buttonClicked={buttonClicked} />
+    </div>
+  );
+};
+
+export default DescobrirEndereco;
