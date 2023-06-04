c7f3cad0ea0927eeb507abdd703fa3502ffe4a9a
Brenno Peralta <brenno.peraltaa@gmail.com>
2023-06-03 21:49:50 -0400

    add: Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./components/Header";
 import Introduction from "./components/Introduction";
 import Forms from "./components/Forms";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Header />
       <Introduction/>
       <Forms/>
+      <Footer/>
     </>
   );
 };
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Footer = () => {
+  return (
+    <footer className="footer-bg d-flex ">
+        <p className="text-m">© 2023 | LocalizaCep</p>
+    </footer>
+  );
+};
+
+export default Footer;
