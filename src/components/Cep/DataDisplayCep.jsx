b74f6a8d551dec2a109343291c56d5a536fb6c2c
Brenno Peralta <brenno.peraltaa@gmail.com>
2023-06-03 21:39:36 -0400

    update: DataDisplayCep component

--- src/components/Cep/DataDisplayCep.jsx
+++ src/components/Cep/DataDisplayCep.jsx
@@ -38,50 +38,51 @@ const DataDisplayCep = ({ rua, cidade, estado, buttonClicked }) => {
     }
   }, [buttonClicked]);
 
-  return(
+  return (
     <>
-    {loading && <p className="text-m">Carregando...</p>}
-    {error && <p className="text-m">{error}</p>}
-    <div className="tabela-cep">
-      {cepList.length > 0 && (
-        <table>
-          <thead>
-            <tr>
-              <th className="hide">Rua</th>
-              <th>Bairro</th>
-              <th className="hide">Cidade</th>
-              <th className="hide">UF</th>
-              <th>CEP</th>
-              <th>Visualizar</th>
-            </tr>
-          </thead>
-          <tbody>
-            {cepList.map((cep) => (
-              <tr key={cep.cep}>
-                <td className="hide">{cep.logradouro}</td>
-                <td>{cep.bairro}</td>
-                <td className="hide">{cep.localidade}</td>
-                <td className="hide">{cep.uf}</td>
-                <td>{cep.cep}</td>
-                <td>
-                  <a className="button-style"
-                    href={`https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(
-                      `${cep.logradouro}, ${cep.bairro}, ${cep.localidade}, ${cep.uf}, ${cep.cep}`
-                    )}`}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Maps
-                  </a>
-                </td>
+      {loading && <p className="text-m">Carregando...</p>}
+      {error && <p className="text-m">{error}</p>}
+      <div className="tabela-cep">
+        {cepList.length > 0 && (
+          <table>
+            <thead>
+              <tr>
+                <th className="hide">Rua</th>
+                <th>Bairro</th>
+                <th className="hide">Cidade</th>
+                <th className="hide">UF</th>
+                <th>CEP</th>
+                <th>Visualizar</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
-    </div>
-  </>
-  )
+            </thead>
+            <tbody>
+              {cepList.map((cep) => (
+                <tr key={cep.cep}>
+                  <td className="hide">{cep.logradouro}</td>
+                  <td>{cep.bairro}</td>
+                  <td className="hide">{cep.localidade}</td>
+                  <td className="hide">{cep.uf}</td>
+                  <td>{cep.cep}</td>
+                  <td>
+                    <a
+                      className="button-style"
+                      href={`https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(
+                        `${cep.logradouro}, ${cep.bairro}, ${cep.localidade}, ${cep.uf}, ${cep.cep}`
+                      )}`}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Maps
+                    </a>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
+      </div>
+    </>
+  );
 };
 
 export default DataDisplayCep;
